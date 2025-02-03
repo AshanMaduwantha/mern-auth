22bf26d393bc63cc61ffa4e6ff4e7af02fd96655
AshanMaduwantha <ashanofficial256@gmail.com>
2025-02-03 13:48:43 +0530

    add

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,11 +21,12 @@ export const register = async (req, res) => {
         
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        constuser = new userModel({
+        const user = new userModel({
             name,
             email,
             password: hashedPassword
         });
+        
         await user.save();
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
@@ -75,6 +76,21 @@ export const login = async (req, res) => {
         });
 
         return res.status(200).json({ success: true, message: "User logged in successfully" });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token", { 
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+
+        return res.status(200).json({ success: true, message: "User logged out successfully" });
 
     } catch (error) {
         return res.status(500).json({ success: false, message: error.message });
