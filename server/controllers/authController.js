c8d45c8fd5f46a4a80c47129109bdb6bd491a94b
AshanMaduwantha <ashanofficial256@gmail.com>
2025-02-03 11:57:43 +0530

    add codes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -44,5 +44,35 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({ success: false, message: "Email and Password are required" });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({ success: false, message: "Invalid Email" });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordMatch) {
+            return res.status(400).json({ success: false, message: "Invalid Password" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.cookie("token", token, { 
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000 
+        });
+
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
 }
